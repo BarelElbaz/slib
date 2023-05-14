8b68b106f6818fe52d213a0b614a20de93258381
Barel Elbaz <barel.elbaz@harmonicinc.com>
2023-05-14 11:33:13 +0300

    TT1

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,4 +1,11 @@
+import org.develeap.Constants
 
-def call(String binaryName){
+def call(String binaryName = Constants.binaryName){
     sh "go build -o $binaryName"
+    sh """#!/usr/bin/env python3
+import datetime
+
+def foo()
+
+    """
 }
