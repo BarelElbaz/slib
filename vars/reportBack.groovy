658262b50f274343b6feb0ef88bb4a7c82fa004e
Barel Elbaz <barel.elbaz@harmonicinc.com>
2023-05-16 01:25:57 +0300

    test emails

--- vars/reportBack.groovy
+++ vars/reportBack.groovy
@@ -1,5 +1,10 @@
-def call(){
-    def template = libraryResource('slack_template.txt')
-    template.replace('<DEV>', "Barel")
-    template.replace('<STATUS>', "PASS")
+def call(def args = [:]){
+    args.body = args.body ?: '${SCRIPT, template="feedback.template"}'
+    args.to = args.to ?: 'jenkins.elbaz@gmail.com'
+    args.subject = args.subject ?: "Jenkins email report ${env.BUILD_NUMBER}"
+    emailext body: "${args.body}",
+        subject: "${args.subject}",
+        mimeType: 'text/html', //important
+        to: "${args.to}",
+        saveOutput: true
 }
--- vars/startPipeline.groovy
+++ vars/startPipeline.groovy
@@ -14,6 +14,14 @@ def call(){
                 }
             }
         }
+        post{
+            always{
+                script{
+                    env.JOB_DATA = '{"builtServices":[{"serviceName":"service1","dockerURI":"006262944085.dkr.ecr.us-east-1.amazonaws.com/service1:1"},{"serviceName":"service2","dockerURI":"006262944085.dkr.ecr.us-east-1.amazonaws.com/service2:1"},{"serviceName":"service3","dockerURI":"006262944085.dkr.ecr.us-east-1.amazonaws.com/service3:1"}]}'
+                    reportBack()
+                }
+            }
+        }
     }
     
 }
