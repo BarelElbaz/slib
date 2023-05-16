b3db9f6d4c7d70a5506922809fc534067e9a146e
Barel Elbaz <barel.elbaz@harmonicinc.com>
2023-05-16 16:00:40 +0300

    Add reports

--- vars/reportBack.groovy
+++ vars/reportBack.groovy
@@ -1,11 +1,10 @@
 def call(def args = [:]){
-    args.to = args.to ?: "jenkins.elbaz@gmail.com"
-
-    emailext body: '${SCRIPT, template="feedback.template"}',
-             to: args.to,
-             subject: 'Jenkins report',
-             mimeType: 'text/html',
-
-
+    args.body = args.body ?: '${SCRIPT, template="feedback.template"}'
+    args.to = args.to ?: 'jenkins.elbaz@gmail.com'
+    args.subject = args.subject ?: "Jenkins email report ${env.BUILD_NUMBER}"
+    emailext body: "${args.body}",
+        subject: "${args.subject}",
+        mimeType: 'text/html', //important
+        to: "${args.to}",
+        saveOutput: true
 }
-
